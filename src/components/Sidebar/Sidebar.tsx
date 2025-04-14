85770fdf4690a1efcb96414c53a41fb686641a6c
Настя <nastia@nastia.local>
2025-04-14 20:15:56 +0300

    update sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,7 @@
 import { memo } from 'react';
 import { SidebarItem } from './components/SidebarItem/SidebarItem';
+import Logo from './../../assets/icons/logo.svg';
+import InfoIcon from './../../assets/icons/Info.svg';
 import * as cls from './Sidebar.module.scss';
 
 type SidebarProps = {
@@ -8,6 +10,11 @@ type SidebarProps = {
 
 export const Sidebar = memo(({list}: SidebarProps) => {
     return <div className={cls.Sidebar}>
+                <div className={cls.logo}><Logo /></div>
                 {list.map(({ title, icon, path }) => <SidebarItem key={title} title={title} Icon={icon} path={path} /> )}
+                <div className={cls.info}>
+                    <div className={cls.info_icon}><InfoIcon /></div>
+                    <div className={cls.info_title}>О компании...</div>
+                </div>
             </div>
 })
