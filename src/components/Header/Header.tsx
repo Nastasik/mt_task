14bd25b4d3a31c50919db443c1918fe538080aaf
Настя <nastia@nastia.local>
2025-04-15 09:04:53 +0300

    header animation and some small fixes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { memo, useCallback, useEffect, useState } from 'react';
 import * as cls from './Header.module.scss';
 
 type HeaderProps = {
@@ -7,7 +7,29 @@ type HeaderProps = {
 }
 
 export const Header =memo(({title, avatar}: HeaderProps) => {
-    return <div className={cls.Header}>
+    const [isHide, setIsHide] = useState('')
+   
+    useEffect(() => {
+        let lastScroll = 0;
+        const defaultOffset = 10;
+         
+        const scrollPosition = () => window.scrollY || document.documentElement.scrollTop;
+
+        const onScroll = () => {
+            const y = scrollPosition()
+            if( y > defaultOffset) {
+                setIsHide('hide');    
+            }
+            else if(y <= lastScroll){
+                setIsHide('emergence');         
+            }
+         
+            lastScroll = scrollPosition();
+        }
+        window.addEventListener('scroll', onScroll)
+    }, [])
+
+    return <div className={`${cls.Header} ${cls[isHide]}`}>
                 <img alt='avatar' src={avatar} className={cls.img}/> 
                 <span className={cls.title}>{title}</span>
             </div>
--- src/pages/LentaPage/LentaPage.tsx
+++ src/pages/LentaPage/LentaPage.tsx
@@ -1,9 +1,10 @@
 import { memo } from "react"
 import { Post } from "./components/Post/Post"
 import { LENTA_DATA } from "./mocks/lenta_data"
+import * as cls from './LentaPage.module.scss';
 
 export const LentaPage = memo(() => {
-    return <div>
+    return <div className={cls.LentaPage}>
                 {LENTA_DATA.map(({ id, image, text, title, date }) => 
                     <Post key={id} title={title} text={text} date={date} image={image} />)}
             </div>
